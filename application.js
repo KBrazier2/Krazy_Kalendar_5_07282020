a54f1fb901ac3cba470bec4891e9559dcd0b2f24
ksbrazier <kylebrazier@gmail.com>
2020-07-26 11:28:58 -0400

    updating to live date/time

--- application.js
+++ application.js
@@ -1,1 +1,9 @@
-document.getElementById("date").innerHTML = Date();
+// current date/time
+// stack overflow URL https://stackoverflow.com/questions/28689524/show-real-time-date-day-of-the-week-in-javascript-or-jquery/28689891
+$(document).ready(function() {
+    var interval = setInterval(function(){
+        var momentNow = moment();
+        $("#date-part").html(momentNow.format("YYYY MMMM DD") + "" + momentNow.format("dddd").substring(0,3).toUpperCase());
+        $("#time-part").html(momentNow.format("A hh:mm:ss"));
+    }, 100);
+})
